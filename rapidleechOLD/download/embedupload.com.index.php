815cd0de8273f97305d9a28d959965308c81e7bf
Bedder <megabedder@hotmail.com>
2015-11-10 05:26:58 -0600

    Add embedupload.com
    Add embedupload.com - Old plugin of Rapidleech

---
+++ rapidleechOLD/download/embedupload.com.index.php
@@ -0,0 +1,6 @@
+<?php
+//For Downloading content go to bulkseries.com
+$upload_services[] = 'embedupload.com';
+$max_file_size['embedupload.com'] = 1024;
+$page_upload['embedupload.com'] = 'embedupload.com.php';
+?>
