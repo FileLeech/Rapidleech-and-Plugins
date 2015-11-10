375232be5da4acb7960900ed4181642a14e71c66
Bedder <megabedder@hotmail.com>
2015-11-10 13:11:53 -0600

    Add mightyupload.com member download plugin
    Add mightyupload.com member download plugin
    
    [05-8-2013] Based on Written by Th3-822
    [04-09-2013] Created by Titeuf

---
+++ rapidleechOLD/download/mightyupload.com.index.php
@@ -0,0 +1,8 @@
+<?php
+$upload_services[] = 'mightyupload.com';
+$max_file_size['mightyupload.com'] = 4096;
+$page_upload['mightyupload.com'] = 'mightyupload.com.php';
+// Based on Written by Th3-822 - Last Update: [05-8-2013]
+// Created by Titeuf [04-09-2013]
+//Work for free registered member, not tested with a premium account (I do not have a premium account at mightyupload.com)
+?>
